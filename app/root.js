d63104fb54084f164498ded7eea10c6b7d1ac17f
xierangh@163.com <xierangh@163.com>
2017-08-02 11:19:01 +0800

    first blood

---
+++ app/root.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import {
+    View,
+    Text,
+}from 'react-native'
+
+export default class root extends React.Component{
+
+    render(){
+        return (
+            <View>
+                <Text>测试</Text>
+            </View>
+        )
+    }
+}
